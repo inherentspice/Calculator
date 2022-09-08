e62aaa8145c585b987939563ca1a779c04d4dd23
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-07 20:18:40 -0700

    completed basic logic of calculator in js

---
+++ calc.js
@@ -0,0 +1,38 @@
+function operate(operator, number1, number2) {
+  if (operator==='+') {
+    return number1 + number2;
+  } else if (operator==='-') {
+    return number1 - number2;
+  } else if (operator==='*') {
+    return number1 * number2;
+  } else if (operator==='/') {
+    return number1 / number2;
+  } else {
+    return 'nope';
+  }
+}
+
+let buttons = document.querySelectorAll('button')
+let operator = null;
+let number1 = null;
+let number2 = null;
+
+buttons.forEach(button => button.addEventListener('click', function() {
+  if (button.className==='operator') {
+    operator = button.innerText;
+  } else if (button.className==='number' && !number1) {
+    number1 = button.innerText;
+  } else if (button.className==='number' && number1) {
+    number2 = button.innerText;
+  } else if (button.className==='equals' && number1 && number2 && operator) {
+    answer = operate(operator, number1, number2);
+    console.log(answer)
+    number1 = answer;
+    operator = null;
+    number2 = null;
+  } else if (button.className==='clear') {
+    operator = null;
+    number1 = null;
+    number2 = null;
+  }
+}))
