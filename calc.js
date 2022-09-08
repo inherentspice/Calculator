5a6752183282f5fc729ebc70289e715bc9bc92b3
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-07 20:44:07 -0700

    added function to update display screen

--- calc.js
+++ calc.js
@@ -12,34 +12,47 @@ function operate(operator, number1, number2) {
   }
 }
 
+function updateScreen() {
+  screenUpdate = document.getElementById('screen');
+  screenDisplay = document.createElement('p');
+  if (operator && number2) {
+    screenDisplay.innerText = `${number1} ${operator} ${number2}`;
+  } else if (operator && !number2) {
+    screenDisplay.innerText = `${number1} ${operator}`;
+  } else if (!operator && !number2) {
+    screenDisplay.innerText = `${number1}`;
+  }
+  screenUpdate.appendChild(screenDisplay);
+};
+
 let buttons = document.querySelectorAll('button')
 let operator = null;
 let number1 = null;
 let number2 = null;
+let answer = null;
+
 
 buttons.forEach(button => button.addEventListener('click', function() {
   if (button.className==='operator') {
     operator = button.innerText;
   } else if (button.className==='number' && !number1) {
     number1 = button.innerText;
-    console.log(number1)
   } else if (button.className==='number' && !operator) {
     number1 += button.innerText;
-    console.log(number1)
   } else if (button.className==='number' && operator && !number2) {
     number2 = button.innerText;
   } else if (button.className==='number' && operator && number2) {
     number2 += button.innerText;
   } else if (button.className==='equals' && number1 && number2 && operator) {
     answer = operate(operator, number1, number2);
-    console.log(answer)
     number1 = answer;
-    console.log(number1)
     operator = null;
     number2 = null;
   } else if (button.className==='clear') {
     operator = null;
     number1 = null;
     number2 = null;
+    answer = null;
   }
+  updateScreen();
 }))
